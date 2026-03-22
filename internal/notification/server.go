5d9b1b950a02170a24f99b519bffdd839b86d536
RadovicDanilo <daniloradovic2003@gmail.com>
2026-03-22 23:58:19 +0100

    refaktorisao notification servis, DRY princip

--- internal/notification/server.go
+++ internal/notification/server.go
@@ -45,152 +45,51 @@ func NewServer(sender EmailSender) *Server {
 	return &Server{sender: sender}
 }
 
-func (s *Server) SendConfirmationEmail(_ context.Context, req *notification.ConfirmationMailRequest) (*notification.SuccessResponse, error) {
-	log.Println("Sending confirmation email")
+func (s *Server) sendEmail(toAddr string, subject string, templateFile string, data interface{}) (*notification.SuccessResponse, error) {
+	to := strings.Split(toAddr, ",")
 
-	to := strings.Split(req.ToAddr, ",")
-
-	templ, err := template.ParseFiles("templates/confirmation.html")
+	templ, err := template.ParseFiles("templates/" + templateFile)
 	if err != nil {
-		log.Println("Cannot parse confirmation.html:", err)
+		log.Printf("Cannot parse %s: %v\n", templateFile, err)
 		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	var rendered bytes.Buffer
-	if err := templ.Execute(&rendered, req); err != nil {
-		log.Println("Cannot execute confirmation.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	err = s.sender.Send(to, "Confirm your Banka 3 account", rendered.String()) //umesto smtp
-	if err != nil {
-		log.Println("Couldn't send confirmation email:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	return &notification.SuccessResponse{
-		Successful: true,
-	}, nil
-}
-
-func (s *Server) SendActivationEmail(_ context.Context, req *notification.ActivationMailRequest) (*notification.SuccessResponse, error) {
-	to := strings.Split(req.ToAddr, ",")
-	templ, err := template.ParseFiles("templates/activation.html")
-	if err != nil {
-		log.Println("Cannot parse activation.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	var rendered bytes.Buffer
-	if err := templ.Execute(&rendered, req); err != nil {
-		log.Println("Cannot execute activation.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	err = s.sender.Send(to, "Aktivirajte Banka 3 nalog", rendered.String()) //umesto smtp
-	if err != nil {
-		log.Println("Couldn't send email:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	return &notification.SuccessResponse{
-		Successful: true,
-	}, nil
-}
-
-func (s *Server) SendPasswordResetEmail(_ context.Context, req *notification.PasswordLinkMailRequest) (*notification.SuccessResponse, error) {
-	to := strings.Split(req.ToAddr, ",")
-	templ, err := template.ParseFiles("templates/password_reset.html")
-	if err != nil {
-		log.Println("Cannot parse password_reset.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	var rendered bytes.Buffer
-	if err := templ.Execute(&rendered, req); err != nil {
-		log.Println("Cannot execute password_reset.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	err = s.sender.Send(to, "Reset your Banka 3 password", rendered.String())
-	if err != nil {
-		log.Println("Couldn't send password reset email:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	return &notification.SuccessResponse{
-		Successful: true,
-	}, nil
-}
-
-func (s *Server) SendInitialPasswordSetEmail(_ context.Context, req *notification.PasswordLinkMailRequest) (*notification.SuccessResponse, error) {
-	to := strings.Split(req.ToAddr, ",")
-	templ, err := template.ParseFiles("templates/initial_password_set.html")
-	if err != nil {
-		log.Println("Cannot parse initial_password_set.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	var rendered bytes.Buffer
-	if err := templ.Execute(&rendered, req); err != nil {
-		log.Println("Cannot execute initial_password_set.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	err = s.sender.Send(to, "Set your Banka 3 password", rendered.String())
-	if err != nil {
-		log.Println("Couldn't send initial password set email:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	return &notification.SuccessResponse{
-		Successful: true,
-	}, nil
-}
-
-func (s *Server) SendCardConfirmationEmail(_ context.Context, req *notification.CardConfirmationMailRequest) (*notification.SuccessResponse, error) {
-	to := strings.Split(req.ToAddr, ",")
-	templ, err := template.ParseFiles("templates/card_confirmation.html")
-	if err != nil {
-		log.Println("Cannot parse car_confirmation.html:", err)
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	data := struct {
-		Link string
-	}{
-		Link: req.Link,
 	}
 
 	var rendered bytes.Buffer
 	if err := templ.Execute(&rendered, data); err != nil {
+		log.Printf("Cannot execute %s: %v\n", templateFile, err)
 		return &notification.SuccessResponse{Successful: false}, nil
 	}
 
-	err = s.sender.Send(to, "Potvrda zahteva za karticu - Banka 3", rendered.String())
+	err = s.sender.Send(to, subject, rendered.String())
 	if err != nil {
+		log.Printf("Couldn't send email (%s): %v\n", templateFile, err)
 		return &notification.SuccessResponse{Successful: false}, nil
 	}
 
 	return &notification.SuccessResponse{Successful: true}, nil
 }
 
+func (s *Server) SendConfirmationEmail(_ context.Context, req *notification.ConfirmationMailRequest) (*notification.SuccessResponse, error) {
+	return s.sendEmail(req.ToAddr, "Confirm your Banka 3 account", "confirmation.html", req)
+}
+
+func (s *Server) SendActivationEmail(_ context.Context, req *notification.ActivationMailRequest) (*notification.SuccessResponse, error) {
+	return s.sendEmail(req.ToAddr, "Aktivirajte Banka 3 nalog", "activation.html", req)
+}
+
+func (s *Server) SendPasswordResetEmail(_ context.Context, req *notification.PasswordLinkMailRequest) (*notification.SuccessResponse, error) {
+	return s.sendEmail(req.ToAddr, "Reset your Banka 3 password", "password_reset.html", req)
+}
+
+func (s *Server) SendInitialPasswordSetEmail(_ context.Context, req *notification.PasswordLinkMailRequest) (*notification.SuccessResponse, error) {
+	return s.sendEmail(req.ToAddr, "Set your Banka 3 password", "initial_password_set.html", req)
+}
+
+func (s *Server) SendCardConfirmationEmail(_ context.Context, req *notification.CardConfirmationMailRequest) (*notification.SuccessResponse, error) {
+	data := struct{ Link string }{Link: req.Link}
+	return s.sendEmail(req.ToAddr, "Potvrda zahteva za karticu - Banka 3", "card_confirmation.html", data)
+}
+
 func (s *Server) SendCardCreatedEmail(_ context.Context, req *notification.CardCreatedMailRequest) (*notification.SuccessResponse, error) {
-	to := strings.Split(req.ToAddr, ",")
-	templ, err := template.ParseFiles("templates/card_created.html")
-	if err != nil {
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	var rendered bytes.Buffer
-	if err := templ.Execute(&rendered, req); err != nil {
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	err = s.sender.Send(to, "Vaša Banka 3 kartica je spremna!", rendered.String())
-	if err != nil {
-		return &notification.SuccessResponse{Successful: false}, nil
-	}
-
-	return &notification.SuccessResponse{Successful: true}, nil
+	return s.sendEmail(req.ToAddr, "Vaša Banka 3 kartica je spremna!", "card_created.html", req)
 }
